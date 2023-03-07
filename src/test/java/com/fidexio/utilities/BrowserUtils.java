54a00063bc94e770c8302e9930d5a25e281b21fa
Mahir KOÇER <mahirkocer@hotmail.com>
2023-03-07 10:03:17 +0300

    verifyTitle method is created

--- src/test/java/com/fidexio/utilities/BrowserUtils.java
+++ src/test/java/com/fidexio/utilities/BrowserUtils.java
@@ -1,4 +1,11 @@
 package com.fidexio.utilities;
 
+import org.junit.Assert;
+
 public class BrowserUtils {
+
+    public static void verifyTitle(String expectedTitle){
+        Assert.assertTrue(Driver.getDriver().getTitle().equals(expectedTitle));
+
+    }
 }
